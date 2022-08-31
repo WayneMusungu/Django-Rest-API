5823c1bcf3bad5a05983fffa1faede1a6343f1e7
WayneMusungu <waynemuyera17@gmail.com>
2022-08-31 15:11:54 +0300

    Refactour views to CBV

--- books/urls.py
+++ books/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
 from . import views
+from books.views import BookDelete, BookDetail, BookList, BookCreate, BookUpdate
 
 
 urlpatterns = [
-    path('', views.bookList),
-    path('create-book/', views.createBook),
-    path('detail-book/<int:id>/', views.detailBook),
-    path('update-book/<int:id>/', views.updateBook),
-    path('delete-book/<int:id>/', views.deleteBook),
+    #FUNCTION BASED VIEWS PATH
+    # path('', views.bookList),
+    # path('create-book/', views.createBook),
+    # path('detail-book/<int:id>/', views.detailBook),
+    # path('update-book/<int:id>/', views.updateBook),
+    # path('delete-book/<int:id>/', views.deleteBook),
+    
+    #CLASS BASED VIEWS PATH
+    path('', BookList.as_view()),
+    path('create-book/', BookCreate.as_view()),
+    path('detail-book/<int:id>/', BookDetail.as_view()),
+    path('update-book/<int:id>/', BookUpdate.as_view()),
+     path('delete-book/<int:id>/', BookDelete.as_view()),
+    
+    
+    
     
 ]
--- books/views.py
+++ books/views.py
@@ -3,10 +3,9 @@ from django.http import JsonResponse
 from .models import Books
 from .seriaizers import BookSerializer
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-
 
 # Create your views here.
 
@@ -16,14 +15,18 @@ def bookList(request):
         books = Books.objects.all()
         serializer = BookSerializer(books, many=True)
         return JsonResponse({"books":serializer.data})
-    
+
+   
 @api_view(['POST'])
 def createBook(request):
     serializer = BookSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+      
 @api_view(['GET'])
 def detailBook(request, id):
     try:
@@ -34,6 +37,7 @@ def detailBook(request, id):
     if request.method == 'GET':
         serializer = BookSerializer(book)
         return Response(serializer.data)
+    
     
 @api_view(['PUT'])
 def updateBook(request, id):
@@ -60,6 +64,64 @@ def deleteBook(request, id):
     if request.method == 'DELETE':
         book.delete()
     return Response('Book deleted successfully')
+
+
+# Class Based Views
+
+class BookList(APIView):
+    def get(self, request):
+        books = Books.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return JsonResponse({"books":serializer.data})
+
+
+class BookCreate(APIView):
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+class BookDetail(APIView):
+    def get(self, request, id):
+        try:
+            book = Books.objects.get(pk=id)
+        except Books.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+        if request.method == 'GET':
+            serializer = BookSerializer(book)
+            return Response(serializer.data)
+        
+        
+class BookUpdate(APIView):
+    def put(self, request, id):
+        try:
+            book = Books.objects.get(pk=id)
+        except Books.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if request.method == 'PUT':
+            serializer = BookSerializer(instance=book, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+        return Response (serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class BookDelete(APIView):
+    def delete(self, request, id):
+        try:
+            book = Books.objects.get(pk=id)
+        except Books.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if request.method == 'DELETE':
+            book.delete()
+        return Response('Book deleted successfully')
 
 
 
